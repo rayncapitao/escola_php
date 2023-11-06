908aea3e3382c7e318b29b93c62f29b33dc72072
rayncapitao <ryancapitao.mm.123@gmail.com>
2023-11-05 21:01:28 -0300

    updated: var names

--- controller/connection.php
+++ controller/connection.php
@@ -4,9 +4,9 @@ $USUARIO = "root";
 $SENHA = "";
 $BASE = "escola_php";
 
-$mysqli = new mysqli($SERVIDOR, $USUARIO, $SENHA, $BASE);
+$conn = new mysqli($SERVIDOR, $USUARIO, $SENHA, $BASE);
 
-if($mysqli -> error) {
-  die("Falha ao conectar no banco de dados: " . $mysqli -> error);
+if($conn -> error) {
+  die("Falha ao conectar no banco de dados: " . $conn -> error);
 }
 ?>
